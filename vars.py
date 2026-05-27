7a9b996a231196ad83c800feb5a00d3cb7c1e6f5
bxbhaihackar <141005552+bxbhaihackar@users.noreply.github.com>
2026-05-28 04:28:30 +0530

    Change BOT_TOKEN to a new value
    Updated BOT_TOKEN to a new value.

--- vars.py
+++ vars.py
@@ -6,4 +6,4 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "25334222"))
 API_HASH = environ.get("API_HASH", "c8ee66889518bd70403e36d54113f745")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8908100142:AAG0JhHB1EQ71rWrTU2WrOmkNKj_u4nk6SY")
